f120a0073d4dcb0612fe67ec0b175fecc58c3840
Dale Foskey <developer.dale.foskey@gmail.com>
2021-04-11 19:10:13 -0400

    Sound board created
    30 days of javascript day 1

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,7 +1,46 @@
-alert("working");
 
-function play() {
-    var audio = document.getElementsByClassName("key");
+
+
+function sixtyFive() {
+    var audio = new Audio('sounds/clap.wav');
     audio.play();
-    <audio data-key="65" src="sounds/clap.wav"></audio>
-  }
+}   
+function eightyThree() {
+    var audio = new Audio('sounds/hihat.wav');
+    audio.play();
+}  
+
+function sixtyEight() {
+    var audio = new Audio('sounds/kick.wav');
+    audio.play();
+}  
+
+function seventy() {
+    var audio = new Audio('sounds/openhat.wav');
+    audio.play();
+}  
+
+function seventyOne() {
+    var audio = new Audio('sounds/boom.wav');
+    audio.play();
+}  
+
+function seventyTwo() {
+    var audio = new Audio('sounds/ride.wav');
+    audio.play();
+}  
+
+function seventyFour() {
+    var audio = new Audio('sounds/snare.wav');
+    audio.play();
+}  
+
+function seventyFive() {
+    var audio = new Audio('sounds/tom.wav');
+    audio.play();
+}  
+
+function seventySix() {
+    var audio = new Audio('sounds/tink.wav');
+    audio.play();
+}  
